2f1738ffe0f8a701e44fb90dabba5004c80b97aa
Erik Ruiter <erikruiter2@gmail.com>
2014-06-27 15:19:29 +0200

    Initial version

---
+++ patchpanel-mgmt.php
@@ -0,0 +1,9 @@
+<?php
+
+$page['patchpanel']['title'] = 'Patchpanels';
+$page['patchpanel']['parent'] = 'index';
+$tab['patchpanel']['default'] = 'Browse';
+$tab['patchpanel']['edit'] = 'Edit';
+
+
+?>
